13581dc2dd5a50d017f3c0fae2371f21faf7aa84
ChloeL19 <cloughridge@college.harvard.edu>
2024-05-10 12:56:10 -0400

    now created a multipage thing hopefully

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 import streamlit as st
 
-# Title of the app
-st.title('Hello, World!')
+# Define a function for each page
+def home():
+    st.title('Home Page')
+    st.write('Welcome to the Home Page!')
+    st.image('assets/bobtail.jpg', caption='a bobtail squid')
 
-# Display an image
-st.image('assets/bobtail.jpg', caption='a bobtail squid')
+def about():
+    st.title('About Page')
+    st.write('This is the About Page.')
 
-# Simple text
-st.write('Hello, Streamlit!')
+def contact():
+    st.title('Contact Page')
+    st.write('This is the Contact Page.')
+
+# Sidebar for navigation
+page = st.sidebar.selectbox('Select a page:', ['Home', 'About', 'Contact'])
+
+# Display the selected page
+if page == 'Home':
+    home()
+elif page == 'About':
+    about()
+elif page == 'Contact':
+    contact()
+
