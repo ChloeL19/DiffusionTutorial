65fbbcb67937c950c3284c480290434ce5a890cb
ChloeL19 <cloughridge@college.harvard.edu>
2024-05-10 12:50:17 -0400

    the beginnings

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Title of the app
+st.title('Hello, World!')
+
+# Display an image
+st.image('assets/bobtail.jpg', caption='a bobtail squid')
+
+# Simple text
+st.write('Hello, Streamlit!')
