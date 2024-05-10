80215bfddfd05fa5571b796df9e9d91c71677ec5
ChloeL19 <cloughridge@college.harvard.edu>
2024-05-10 13:21:58 -0400

    writing out the intro to my notes

--- app.py
+++ app.py
@@ -4,7 +4,24 @@ import streamlit as st
 def home():
     st.title('Overview')
     st.write('What is Diffusion?')
+    st.header('Notes on online lectures')
+    st.write(
+        """What if, given a set of images, we could create--from seemingly thin air-- an image that looks like it should belong exactly in that set?
+        In mathematical language, this question is asking if we can efficiently learn to draw a sample from a given distribution of images.
+        Sampling from some crazy distribution over images, you say? But don't we know how to sample from any sort of crazy distribution? Use Markov Chain Monte Carlo (MCMC) [separate blog post coming soon].
+        Constructing an MCMC transition matrix for the space of images, however, is an extreme computational feat. How do you think about structuring the graph that you will randomly walk along? If each node is an image with a slightly different set of pixel values, that is an extremely dense graph.
+        Instead, perhaps we can sample from a well-known distribution, and somehow construct a function that maps the well known distribution into the distribution we actually care about.
+        This is what diffusion models do.
+        
+        While we're still talking at the high level, here, it's worth noting that 
+        """
+    )
+    st.write(
+    """Compression is intelligence. Previous work on variational autoencoders reveals we can store compressed representations of data in a latent space.
+    See my post on how variational autoencoders work here: [LINK]. So what if we perform diffusion not in pixel space but in latent space?
+    """)
     st.header('Questions Confusing Chloe')
+    st.write('How do you define MCMC over graphs in which neighbors vary with continuous values? i.e. in which neighbors are images with pixel values that differ by an differentiably small amount?')
 
 def browse_examples():
     st.title('Browse Examples')
